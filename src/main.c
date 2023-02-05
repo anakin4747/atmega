e608214536a6a3804b2d495e831f1e16f2adbac8
anakin4747 <anakin@childerhose.ca>
2023-02-04 19:56:18 -0500

    Merged to master

--- src/main.c
+++ src/main.c
@@ -11,10 +11,10 @@ int main(void){
     while(1){
         PORTB |= 1 << PORTB5;
 
-        _delay_ms(300);
+        _delay_ms(3000);
 
         PORTB &= ~(1 << PORTB5);
 
-        _delay_ms(300);
+        _delay_ms(3000);
     }
 }
