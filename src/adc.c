2c7e982c04721a56c832860fa0b85a8198dd9e4e
anakin4747 <anakin@childerhose.ca>
2023-02-07 16:40:20 -0500

    Incompelete ADC stuff: '

--- src/adc.c
+++ src/adc.c
@@ -6,17 +6,31 @@ volatile uint16_t adc_result;
 
 ISR(ADC_vect){
 
-    adc_result = ADC;
+    adc_result = ADCL;
+    adc_result |= (ADCH << 8);
 
 }
 
 void adc_init(void){
+
+    // Set the ADC prescaler to 128, to give a 125 kHz ADC clock frequency
     ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 
+    // Set the voltage ref to internal 1.1V
+    ADMUX |= (1 << REFS1) | (1 << REFS0);
+
+    // Set ADC pin as input
+    DDRC &= ~(1 << DDC0);
+
+    // The power reduction bit must be set to 0 to enable 
+    PRR &= ~(1 << PRADC);
+
+    // ADC enable bit and interrupt bit
     ADCSRA |= (1 << ADEN) | (1 << ADIE);
 
+    // Enable global interrupts
     sei();
 
+    // Start the first conversion
     ADCSRA |= (1 << ADSC);
 }
-
--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 #include "../include/adc.h"
 
 
-#define LED_PIN PB5
-
 volatile uint16_t adc_result;
 
 int main(void){
@@ -14,8 +12,7 @@ int main(void){
     adc_init();
 
     // Set the LED pin as an output
-    DDRB |= (1 << LED_PIN);
-    DDRC &= ~(1 << DDC0);
+    DDRB |= (1 << PB5);
 
     // Enable global interrupts
     sei();
@@ -24,17 +21,15 @@ int main(void){
         // Wait for the ADC conversion to complete
         while (!(ADCSRA & (1 << ADIF)));
 
-        // Read the ADC result
-        uint16_t adc_result = ADC;
         uint16_t Vref = 5;
 
         uint16_t voltage = (adc_result * Vref) / 1024;
 
         // Turn on the LED if the ADC result is above a threshold
         if (voltage > 1) {
-            PORTB |= (1 << LED_PIN);
+            PORTB |= (1 << PB5);
         } else {
-            PORTB &= ~(1 << LED_PIN);
+            PORTB &= ~(1 << PB5);
         }
 
         // Clear the ADC interrupt flag
